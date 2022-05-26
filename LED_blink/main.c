58b52a425b1de46066e0665f1cbbad28b0ef0bb2
ImpulseImp <ImpulsEmbedded@gmail.com>
2022-05-26 14:52:43 +0300

    Update main.c

--- LED_blink/main.c
+++ LED_blink/main.c
@@ -7,7 +7,7 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>   
-#define LED PB0 // 
+#define LED PB0 // connect an LED to PINB0 (pin 8 on an Arduino board) 
 #define freacuancy 250 //2 Hz by default (1000=1sec)
 #define LED_on PORTB |= (1<<LED);  //set led bit to PORTB
 #define LED_off PORTB &= ~(1<<LED); //clear led bit from PORTB
